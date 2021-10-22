eb26d09ee2cbf84a98225946b626900fc240cd5c
EdisonStalin[C <edison.jumbo@epn.edu.ec>
2021-10-22 15:57:48 -0500

    HistoryTrips

--- drivers_app/lib/DataHandler/appData.dart
+++ drivers_app/lib/DataHandler/appData.dart
@@ -1,21 +1,50 @@
+import 'package:drivers_app/Models/history.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:drivers_app/Models/address.dart';
 
 
 class AppData extends ChangeNotifier
 {
-  Address pickUpLocation , dropOffLocation;
+  //Address pickUpLocation , dropOffLocation;
 
-  void updatePickUpLocationAddress(Address pickUpAddress)
+  //void updatePickUpLocationAddress(Address pickUpAddress)
+ // {
+   // pickUpLocation = pickUpAddress;
+    //notifyListeners();
+
+  //}
+  //void updateDropOffLocationAddress(Address dropOffAddres)
+ // {
+   // dropOffLocation = dropOffAddres;;
+   // notifyListeners();
+ // }
+
+  String earnings = "0";
+  int countTrips = 0;
+  List<String> tripHistoryKeys = [];
+  List<History> tripHistoryDataList = [];
+
+  void updateEarnings(String updatedEarnings)
   {
-    pickUpLocation = pickUpAddress;
+    earnings = updatedEarnings;
     notifyListeners();
-
   }
-  void updateDropOffLocationAddress(Address dropOffAddres)
-  {
-    dropOffLocation = dropOffAddres;;
-    notifyListeners();
 
+  void updateTripsCounter(int tripCounter)
+  {
+    countTrips = tripCounter;
+    notifyListeners();
+  }
+
+  void updateTripKeys(List<String> newkeys)
+  {
+    tripHistoryKeys = newkeys;
+    notifyListeners();
+  }
+
+  void updateTripHistoryData(History eachHistory)
+  {
+    tripHistoryDataList.add(eachHistory);
+    notifyListeners();
   }
 }
